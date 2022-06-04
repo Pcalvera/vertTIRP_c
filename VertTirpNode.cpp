fc5355f1f270b0cc29548c39a0435651ab5fb0de
pau <paucalvera@gmail.com>
2022-06-04 10:22:32 +0200

    VertTirpNode.h començat

--- VertTirpNode.cpp
+++ VertTirpNode.cpp
@@ -4,10 +4,38 @@
 
 #include "VertTirpNode.h"
 
-VertTirpNode::VertTirpNode() {
-
+VertTirpNode::VertTirpNode(string &patt, unsigned int pat_len,  VertTirpSidList sidList, bool is_root) {
+    this->patt = patt;
+    this->pat_len = pat_len;
+    this->sidlist = sidList;
+    this->is_root = is_root;
+    this->child,this->brother = nullptr;
 }
 
-VertTirpNode::VertTirpNode(vector<char> &patt, unsigned int pat_len, VertTirpNode &parent, VertTirpSidList sidList) {
+void VertTirpNode::add_child(shared_ptr<VertTirpNode> &ch) {
+    if (this->child == nullptr)
+        this->child = ch;
+    else
+        this->child->add_brother(ch);
+}
 
+void VertTirpNode::add_childs(vector<shared_ptr<VertTirpNode>> &ch) {
+    if ( ch.size() > 0 ) {
+        this->child = ch[0];
+        this->child->add_brothers(ch,1);
+    }
+}
+
+void VertTirpNode::add_brother(shared_ptr<VertTirpNode> &ch) {
+    if ( this->brother == nullptr )
+        this->brother = ch;
+    else
+        this->brother->add_brother(ch);
+}
+
+void VertTirpNode::add_brothers(vector<shared_ptr<VertTirpNode>> &ch, int i) {
+    if ( i < ch.size() ){
+        this->brother = ch[i];
+        this->brother->add_brothers(ch,i+1);
+    }
 }
--- VertTirpNode.h
+++ VertTirpNode.h
@@ -6,15 +6,38 @@
 #define VERTTIRP_C_VERTTIRPNODE_H
 #include <iostream>
 #include <vector>
+#include <memory>
+#include "VertTirpSidList.h"
 #include "VertTirpSidList.h"
 
 using namespace std;
 
 class VertTirpNode {
 public:
-    VertTirpNode();
-    VertTirpNode(vector<char> &patt, unsigned pat_len,VertTirpNode &parent, VertTirpSidList sidList);
+    VertTirpNode(string &patt, unsigned pat_len, VertTirpSidList sidList, bool is_root=true);
+    void add_child(shared_ptr<VertTirpNode> &ch );
+    void add_childs(vector<shared_ptr<VertTirpNode>> &ch );
 private:
+    void add_brother(shared_ptr<VertTirpNode> &ch );
+    void add_brothers(vector<shared_ptr<VertTirpNode>> &ch,int i);
+
+
+
+    //struct Node {
+    //    //TODO de moment prescideixo de l'atribut parent
+    //    shared_ptr<Node> child,brother;
+    //    string patt;
+    //    unsigned pat_len;
+    //    VertTirpSidList sidlist;
+    //};
+    //shared_ptr<Node> node;
+    //bool is_root;
+    //TODO de moment prescideixo de l'atribut parent
+    shared_ptr<VertTirpNode> child,brother;
+    string patt;
+    unsigned pat_len;
+    VertTirpSidList sidlist;
+    bool is_root;
 };
 
 
