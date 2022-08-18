103a4b96acfd079bd80924fa9095901008393b2f
Pcalvera <paucalvera@gmail.com>
2022-08-18 17:21:08 +0200

    chrono

---
+++ Chrono.h
@@ -0,0 +1,20 @@
+#ifndef VERTTIRP_C_CHRONO_CPP
+#define VERTTIRP_C_CHRONO_CPP
+#include <iostream>
+#include <iomanip>
+#include <chrono>
+#include "map"
+using namespace std;
+class Chrono {
+public:
+    Chrono();
+    void start(string name);
+    void stop(string name);
+    void print()const;
+
+    map<string,double> t;
+    map<string,chrono::time_point<chrono::high_resolution_clock>> taux;
+};
+
+
+#endif //VERTTIRP_C_CHRONO_CPP
